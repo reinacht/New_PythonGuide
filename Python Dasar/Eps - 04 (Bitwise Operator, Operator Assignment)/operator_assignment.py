f68af9d8a3ae32cc5b8481c3cb6ebe807b18d558
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-02 19:57:41 +0700

    Update operator_assignment.py

--- Python Dasar/Eps - 04 (Bitwise Operator, Operator Assignment)/operator_assignment.py
+++ Python Dasar/Eps - 04 (Bitwise Operator, Operator Assignment)/operator_assignment.py
@@ -59,66 +59,7 @@ print(z,"binernya :",format(z, '08b'), "digeser 2 kali kekanan")
 z = 7
 print(z,"binernya :",format(z, '08b'))
 z <<= 2
-=======
-# operator assignment
-# penyederhanaan operator (semua operator bisa 
-
-
-# aritmatika
-a = 10
-a = a + 1
-print(a) #11
-
-a = 10
-a += 1
-print(a) #11
-
-a = 10
-a -= 1
-print(a) #9
-
-a = 10
-a *= 2
-print(a) #20
-
-a = 10
-a /= 2
-print(a) #5
-
-a = 10
-a %= 3
-print(a) #1
-
-a = 10
-a //= 3
-print(a) #3
-
-a = 10
-a **= 3
-print(a) #100
-
-
-# logika
-x = True
-x |= True # x = x | True
-print(x)
-
-x = True
-x &= False
-print(x)
-
-x = True
-x ^= True
-print(x)
-
-# geser (shift right and left)
-z = 7
-print(z,"binernya :",format(z, '08b'))
-z >>= 2 # z = z >> 2 (geser angka biner sebanyak 2 Langkah ke kanan)
-print(z,"binernya :",format(z, '08b'), "digeser 2 kali kekanan")
-
-z = 7
-print(z,"binernya :",format(z, '08b'))
-z <<= 2
->>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
 print(z,"binernya :",format(z, '08b'), "digeser 2 kali kekiri")
+
+
+>>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
