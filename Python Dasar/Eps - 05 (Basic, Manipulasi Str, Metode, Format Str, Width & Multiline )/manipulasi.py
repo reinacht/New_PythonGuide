aaebe0f0c9be72c4741e5af52a208bd37b3916ac
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-02 20:01:34 +0700

    Update manipulasi.py

--- Python Dasar/Eps - 05 (Basic, Manipulasi Str, Metode, Format Str, Width & Multiline )/manipulasi.py
+++ Python Dasar/Eps - 05 (Basic, Manipulasi Str, Metode, Format Str, Width & Multiline )/manipulasi.py
@@ -45,52 +45,6 @@ print("Char utk 170 : ", data)
 # 4. Metode Str
 text = "kuku kaki kakak dan kakek adalah jumlah dari kuku kaki nenek"
 count = text.count("k")
-=======
-# manipulasi string
-
-# 1. Menyambung String (concatenate) 
-nama_dpn = "Rei"
-nama_blkng = "Ayanami"
-full_name = nama_dpn + " " + nama_blkng
-
-print("Nama Lengkapnya : ", full_name)
-
-# 2. Mengukur panjang Str
-name = "Lelouch VI Britannia"
-length = len(name)
-print("Panjang String : ", length)
-
-# 3. Operator untuk Str
-
-# cek ketersediaan
-check = "VI" in name
-print("Ketersediaan : ", check)
-check = "Great" not in name
-print("Ketersediaan : ", check)
-
-# pengulangan
-print("Wleo"*10)
-print(15*"Rero")
-
-# indexing
-print("Index ke-0 : ", name[0])
-print("Index ke-(-1) : ", name[-1])
-print("Index ke-[0:6] : ", name[0:7]) # range index
-print("Index ke-[0,2,4,6,8,10] :", name[0:11:2]) # index genap
-
-# max-min item
-print("Min : ", min(name))
-print("Max : ", max(name))
-
-# cek ascii
-ascii_code = str(ord(" ")) 
-print("ASCII Code utk spasi : ", ascii_code)
-data = chr(170) 
-print("Char utk 170 : ", data)
-
-
-# 4. Metode Str
-text = "kuku kaki kakak dan kakek adalah jumlah dari kuku kaki nenek"
-count = text.count("k")
->>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
 print("Jumlah huruf K dari kalimat", text, "adalah", count)
+
+>>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
