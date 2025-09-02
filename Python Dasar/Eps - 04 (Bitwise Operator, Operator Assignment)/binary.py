f74c91fc08935877f8d9eaa15f87126c142f4638
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-02 19:56:26 +0700

    Update binary.py

--- Python Dasar/Eps - 04 (Bitwise Operator, Operator Assignment)/binary.py
+++ Python Dasar/Eps - 04 (Bitwise Operator, Operator Assignment)/binary.py
@@ -4,11 +4,6 @@ x = 1000
 while i < x:
     i = i + 1
     result = format(i, '08b')
-=======
-i = 0
-x = 1000
-while i < x:
-    i = i + 1
-    result = format(i, '08b')
->>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
     print(i, "=", result)
+
+>>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
