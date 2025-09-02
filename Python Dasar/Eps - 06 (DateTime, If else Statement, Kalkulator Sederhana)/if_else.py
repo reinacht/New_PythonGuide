9d1bc125ebbc6e2e760b6c725bfa47492a1f6096
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-02 20:05:30 +0700

    Update if_else.py

--- Python Dasar/Eps - 06 (DateTime, If else Statement, Kalkulator Sederhana)/if_else.py
+++ Python Dasar/Eps - 06 (DateTime, If else Statement, Kalkulator Sederhana)/if_else.py
@@ -18,27 +18,6 @@ elif name == "Rei":
 # 3. Else
 else:
     print(f"Maaf {name}, Mungkin Anda yang ke-terakhir atau Kami yang tidak mengenali Anda")
-    
-=======
-# Flow Code (If, Else, Elif statement)
-# If
-# Konditionen
-# Aktionen
-
-
-# 1. If
-name = input("Masukkan Nama : ")
-print(name)
-if name == "Barok":
-    print(f"Hai {name}, Anda berarti yang Utama!")
-# 2. Elif
-elif name == "DorberkW":
-    print(f"Hai {name}, Anda yang paling pintar di sini! Halo sekali lagi Jenius!")
-elif name == "Rei":
-    print(f"Hai {name}, Anda lagi naik daun, kan?")
-# 3. Else
-else:
-    print(f"Maaf {name}, Mungkin Anda yang ke-terakhir atau Kami yang tidak mengenali Anda")
+print("Selamat Malam, Sayonara!")
     
 >>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
-print("Selamat Malam, Sayonara!")
