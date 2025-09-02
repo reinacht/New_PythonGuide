0b52163a625e85a31610f03406d12f217ad059a5
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-02 22:53:01 +0700

    Update triangle.py

--- Python Dasar/Eps - 07 (Looping - For and While, Continue, Pass, Break/triangle.py
+++ Python Dasar/Eps - 07 (Looping - For and While, Continue, Pass, Break/triangle.py
@@ -16,23 +16,6 @@ while True:
 
     if count > alas:
         break
-=======
-# TRIANGLE MAKER
-print(10*":",'Triangle',10*":")
 
-alas =  int(input("Masukkan Alas : "))
-spasi = int(alas / 2)
-count = 1
-
-while True:
-    if count%2 != 0:
-        print(" "*spasi,'^'*count)
-        count += 1
-    else:
-        count += 1
-        spasi -= 1
-
-    if count > alas:
-        break
 >>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
         
