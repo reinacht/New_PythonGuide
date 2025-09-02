f274ff4ce64de8d06863b0aa49d0db20fe27e658
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-02 19:51:27 +0700

    Update aritmatika.py

--- Python Dasar/Eps - 02 (Input, Aritmatika, Perhitungan Suhu)/aritmatika.py
+++ Python Dasar/Eps - 02 (Input, Aritmatika, Perhitungan Suhu)/aritmatika.py
@@ -43,50 +43,6 @@ y = 9
 z = 2
 
 hasil = y ** x + z % 1 - x // z
-=======
-# aritmatika dalam pyhton ada 7
-# +   = penjumlahan
-a = 3
-b = 7
-
-hasil = a + b
-print(a, '+', b, '=', hasil)
-
-# -   = pengurangan
-hasil = a - b
-print(a, '-', b, '=', hasil)
-
-# *   = perkalian
-hasil = a * b
-print(a, '*', b, '=', hasil)
-
-# /   = pembagian
-hasil = a / b
-print(a, '/', b, '=', hasil)
-
-# **  = perpangkatan
-hasil = a ** b
-print(a, '**', b, '=', hasil)
-
-# %   = modulus, hasil sisa bagi (10 / 3 = 3 dan bersisa 1) = (10 % 3 = 1)
-hasil = a % b
-print(a, '%', b, '=', hasil)
-
-# //  = floor division, hasil bagi yang dibulatkan ke bawah
-hasil = a // b
-print(a, '//', b, '=', hasil)
-
-
-# priority
-# 1. ()
-# 2. eksponen = **
-# 3. perkalian dan sebagainya = * / % //
-# 4. penjumlahan dan pengurangan = + -
-
-x = 3
-y = 9
-z = 2
-
-hasil = y ** x + z % 1 - x // z
->>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
 print('Hasil dari', y, '**', x, '+', z, '% 1 -', x, '//', z, ' : ', hasil)
+
+>>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
