2a485d39986598c6e1efe509d5248a38f5d94e16
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-06 23:09:44 +0700

    Update typehints.py

--- Python Dasar/Eps - 11 (Def or Function)/typehints.py
+++ Python Dasar/Eps - 11 (Def or Function)/typehints.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 '''Type Hints'''
 # adalah variabel dgn jenis tertentu yang dimasukkan ke dalam fungsi
 
@@ -23,32 +22,5 @@ def printText(text:str, num:int) -> str: # berarti 1 argumen str, 1 int dan outp
     numStr = str(num)
     hasil = text, numStr
     return hasil
-    
-=======
-'''Type Hints'''
-# adalah variabel dgn jenis tertentu yang dimasukkan ke dalam fungsi
 
-# Kasus 1
-def count(num:int): # berarti argumennya harus tertipe int
-    hasil = num*9
-    return hasil
-    
-print(count(9))
-
-
-# Kasus 2
-def expand(num:int, square:int) -> int: # berarti argumen dan output returnnya harus bertipe int
-    hasil = num**square
-    return hasil
-    
-print(expand(7, 8))
-
-
-# Kasus 3
-def printText(text:str, num:int) -> str: # berarti 1 argumen str, 1 int dan output returnnya harus bertipe str 
-    numStr = str(num)
-    hasil = text, numStr
-    return hasil
-    
->>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
 print(printText("Lunar", 7))
