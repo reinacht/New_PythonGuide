d828207d9a63737a679dfb7cf9daa084b0ee5086
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-06 22:56:54 +0700

    Update listpy.py

--- Python Dasar/Eps - 08 (List)/listpy.py
+++ Python Dasar/Eps - 08 (List)/listpy.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # list = array
 
 # berbagai value
@@ -19,26 +18,4 @@ range_list = [i for i in range(0,100)]
 print(range_list)
 
 cust_list = [i for i in range(0,100) if i%2 == 0]
-=======
-# list = array
-
-# berbagai value
-num_list = [0, 1, 3, 5, 5]
-print(num_list)
-
-str_list = ['BA', 'BI', 'ANJ', 'ING']
-print(str_list)
-
-bool_list = [True, False, True, False]
-print(bool_list)
-
-mix_list = [7, 'Rei', True]
-print(mix_list)
-
-# buat list
-range_list = [i for i in range(0,100)]
-print(range_list)
-
-cust_list = [i for i in range(0,100) if i%2 == 0]
->>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
 print(cust_list)
