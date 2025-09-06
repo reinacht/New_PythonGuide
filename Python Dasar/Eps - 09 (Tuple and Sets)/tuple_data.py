45ddd52a238c2c64206d52bc0e3a5d7144173129
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-06 23:00:31 +0700

    Update tuple_data.py

--- Python Dasar/Eps - 09 (Tuple and Sets)/tuple_data.py
+++ Python Dasar/Eps - 09 (Tuple and Sets)/tuple_data.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # tuple adalah salah satu data collections py yang valuenya tidak bisa diubah dan bisa dimanipulasi (add n remove), 
 # tetapi ada indexnya
 
@@ -9,16 +8,5 @@ print(tuple_data[0])
 
 # bisa semua jenisn value dan dapat diduplikasi
 mix_tuple = (3, 5, 1, False, True, 'ear', 0, 'arin', 'arin')
-=======
-# tuple adalah salah satu data collections py yang valuenya tidak bisa diubah dan bisa dimanipulasi (add n remove), 
-# tetapi ada indexnya
 
-tuple_data = (3, 5, 6, 7, 8, 4)
-print(tuple_data)
-
-print(tuple_data[0])
-
-# bisa semua jenisn value dan dapat diduplikasi
-mix_tuple = (3, 5, 1, False, True, 'ear', 0, 'arin', 'arin')
->>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
 print(mix_tuple)
