0f030acbf5acfd817d077ef03baa9cebbbb1a36d
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-06 23:06:47 +0700

    Update def0Rfunction.py

--- Python Dasar/Eps - 11 (Def or Function)/def0Rfunction.py
+++ Python Dasar/Eps - 11 (Def or Function)/def0Rfunction.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # pengenalan fungsi
 
 def mencetak():
@@ -40,54 +39,3 @@ result = count()
 print(result)
 result = count(angka2=44)
 print(result)
-
-
-
-
-=======
-# pengenalan fungsi
-
-def mencetak():
-    print("Ini fungsi sederhana")
-
-mencetak()
-
-
-# fungsi dengan argumen
-def cetak(angka):
-    hasil = angka - 999
-    print(hasil)
-
-cetak(7)
-
-
-# fungsi dengan return
-def hitung(angka1, angka2, angka3):
-    hasil = angka1 * angka2 * angka3
-    return hasil
-
-result = hitung(3, 8, 9)
-print(result)
-
-
-# fungsi dengan default argument
-def count(angka1, angka2, angka3=69):
-    hasil = angka1 * angka2 * angka3
-    return hasil
-
-result = count(3, 8)
-print(result)
-
-def count(angka1=55, angka2=64, angka3=69):
-    hasil = angka1 * angka2 * angka3
-    return hasil
-
-result = count()
-print(result)
-result = count(angka2=44)
-print(result)
-
-
-
-
->>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
