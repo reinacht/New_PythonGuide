0969fe6e35ce07383e8fd75861c644c3ba47c75c
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-06 22:58:38 +0700

    Update nested_list_and_deepcopy.py

--- Python Dasar/Eps - 08 (List)/nested_list_and_deepcopy.py
+++ Python Dasar/Eps - 08 (List)/nested_list_and_deepcopy.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # List yang bersarang
 # Nested - List harus menggunakan .deepcopy()
 name = ['Ghua', 'Lubi', 'Yuji', 'Rwuga', 'Vierra']
@@ -16,23 +15,4 @@ print(f"identity dc : {hex(id(dc_list))} ")
 print(f"identity nest : {hex(id(nest_list))} ")
 
 print(f"identity elemen 0 dc : {hex(id(dc_list[0]))} ")
-=======
-# List yang bersarang
-# Nested - List harus menggunakan .deepcopy()
-name = ['Ghua', 'Lubi', 'Yuji', 'Rwuga', 'Vierra']
-age = [9, 11, 45, 23, 18]
-nest_list = [name, age]
-
-print(nest_list)
-
-# .deepcopy() = meng-copy secara menyeluruh
-from copy import deepcopy
-dc_list = deepcopy(nest_list)
-
-print(dc_list)
-print(f"identity dc : {hex(id(dc_list))} ")
-print(f"identity nest : {hex(id(nest_list))} ")
-
-print(f"identity elemen 0 dc : {hex(id(dc_list[0]))} ")
->>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
 print(f"identity elemen 0 nest : {hex(id(nest_list[0]))} ")
