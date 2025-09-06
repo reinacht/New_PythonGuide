f384352a6407245f1a5b44e2994527211cd9e34f
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-06 23:08:23 +0700

    Update global_local.py

--- Python Dasar/Eps - 11 (Def or Function)/global_local.py
+++ Python Dasar/Eps - 11 (Def or Function)/global_local.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # global scope adalah cara mengambil variabel dari environment global ke dlm fungsi
 
 name_global = 'Rei' # <-- global variable (bisa diakses di mana pun)
@@ -40,47 +39,4 @@ if True:
     numD = 999
 
 print(num)
-=======
-# global scope adalah cara mengambil variabel dari environment global ke dlm fungsi
-
-name_global = 'Rei' # <-- global variable (bisa diakses di mana pun)
-
-## akses
-def cetakG():
-    print(f"{name_global} merupakan variabel global")
-cetakG()
-
-def cetakL():
-    name_local = 'Yui' # <-- local variable (hnya dpt diakses di dlm fungsi deklarasi)
-    print(f"{name_local} merupakan variabel local")
-cetakL()
-
-
-## ubah
-def ubahG(): # selain dari parameter dn argumen kita bisa memasukkan var dgn global 
-    global varG # mengakses varG
-    varG = 'Rutera'
-    return varG
-
-varG = 'Elair'
-print(f"Sebelum : {varG}")
-print(f"Sesudah : {ubahG()}")
-
-
-## var local di if dan for bisa diakses
-num = 0
-for nums in range(0,7):
-    num += nums
-    numD = 99
-
-print(num)
-print(numD)
-
-
-if True:
-    num = 111
-    numD = 999
-
-print(num)
->>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
 print(numD)
