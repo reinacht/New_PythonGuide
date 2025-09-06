9a2be9a058509b5c38dcbdc3bb9760a8c097d288
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-06 23:02:34 +0700

    Update dictionary_dasar.py

--- Python Dasar/Eps - 10 (Dictionary)/dictionary_dasar.py
+++ Python Dasar/Eps - 10 (Dictionary)/dictionary_dasar.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # dictionary adalah data collections yang populer di python
 # dapat menyimpan banyak jenis data
 
@@ -18,25 +17,4 @@ hewan = {
 }
 
 print(hewan)
-=======
-# dictionary adalah data collections yang populer di python
-# dapat menyimpan banyak jenis data
-
-# bentuk umum dictionary
-dict_o = {
-    'key' : 'value'
-}
-
-print(dict_o)
-
-
-# dict berisi list
-list_hewan = ['anjing', 'babi', 'biawak']
-hewan = {
-    'kat1' : list_hewan,
-    'kat2' : 'kudanil, berang berang, hiu'
-}
-
-print(hewan)
->>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
 print(f'Kategori 1 : {hewan['kat1']}')
