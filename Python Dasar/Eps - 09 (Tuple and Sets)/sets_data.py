b7abfec3d7ff187ae27fde0c30cdbd93374e5637
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-06 23:00:01 +0700

    Update sets_data.py

--- Python Dasar/Eps - 09 (Tuple and Sets)/sets_data.py
+++ Python Dasar/Eps - 09 (Tuple and Sets)/sets_data.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # sets adalah salah satu data collections py yang valuenya otomatis diurutkan dan tidak ada index, 
 # tetapi hanya bisa add and remove
 
@@ -13,20 +12,4 @@ print(sets_data)
 
 # valuenya bisa semua, tapi ga boleh duplikat (akan dianggap 1)
 mix_sets = {12, 'erin', 89, False, True, 'tyon', 'erin'}
-=======
-# sets adalah salah satu data collections py yang valuenya otomatis diurutkan dan tidak ada index, 
-# tetapi hanya bisa add and remove
-
-sets_data = {9, 5, 2, 6, 0}
-print(sets_data)
-
-sets_data.add(17)
-print(sets_data)
-
-sets_data.remove(17)
-print(sets_data)
-
-# valuenya bisa semua, tapi ga boleh duplikat (akan dianggap 1)
-mix_sets = {12, 'erin', 89, False, True, 'tyon', 'erin'}
->>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
 print(mix_sets)
