d5d694cdd6f3b365ec004ccfe0cb1e9836debbc2
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-06 23:09:12 +0700

    Update rekursi.py

--- Python Dasar/Eps - 11 (Def or Function)/rekursi.py
+++ Python Dasar/Eps - 11 (Def or Function)/rekursi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def fib(x):
     if x < 2:
         return x
@@ -6,13 +5,4 @@ def fib(x):
 
 num = int(input("\nMasukkan urutannya : "))
 fibonaci_ke = fib(num)
-=======
-def fib(x):
-    if x < 2:
-        return x
-    return fib(x-1) + fib(x-2)
-
-num = int(input("\nMasukkan urutannya : "))
-fibonaci_ke = fib(num)
->>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
 print(f"Bilangan fibonacci ke-{num} adalah {fibonaci_ke}\n")
