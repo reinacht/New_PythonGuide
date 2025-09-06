0c54a439c72b93e777348b34e30996a454d8220b
Barok <133126253+reinacht@users.noreply.github.com>
2025-09-06 22:55:50 +0700

    Update copy_list.py

--- Python Dasar/Eps - 08 (List)/copy_list.py
+++ Python Dasar/Eps - 08 (List)/copy_list.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Cara menduplikat list yang benar adalah menggunakan list.copy()
 # Cara yang salah
 
@@ -22,34 +21,3 @@ print(f"z : {z}")
 print(f"addres x :  {hex(id(x))}")
 print(f"addres y :  {hex(id(y))}")
 print(f"addres y :  {hex(id(z))}")
-
-
-
-=======
-# Cara menduplikat list yang benar adalah menggunakan list.copy()
-# Cara yang salah
-
-x = [0, 0, 1, 10, 224, 4]
-y = x
-
-print(f"x : {x}")
-print(f"y : {y}")
-
-print(f"addres x :  {hex(id(x))}")
-print(f"addres y :  {hex(id(y))}")
-
-
-# Cara yang benar
-z = x.copy()
-
-print(f"x : {x}")
-print(f"y : {y}")
-print(f"z : {z}")
-
-print(f"addres x :  {hex(id(x))}")
-print(f"addres y :  {hex(id(y))}")
-print(f"addres y :  {hex(id(z))}")
-
-
-
->>>>>>> cd1aaeb9d87aeb86de2fca0ed17417b15189fc16
